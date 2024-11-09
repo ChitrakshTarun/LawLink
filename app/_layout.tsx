2c7148c52b461e428b2e21397d7860d728ef2da3
Chitraksh Tarun <65770420+ChitrakshTarun@users.noreply.github.com>
2024-11-10 05:03:29 +0530

    chore(deps): remove unused dependencies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,45 +1,40 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@/global.css";
 import { Auth0Provider } from "@/providers/Auth0Provider";
-
-const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
     <Auth0Provider>
-      <QueryClientProvider client={queryClient}>
-        <GestureHandlerRootView>
-          <StatusBar barStyle={"dark-content"} />
-          <Stack>
-            <Stack.Screen
-              name="(tabs)"
-              options={{
-                headerShown: false,
-              }}
-            />
-            <Stack.Screen
-              name="chat"
-              options={{
-                headerShown: false,
-                animation: "slide_from_bottom",
-                headerBackTitle: "Back",
-              }}
-            />
-            <Stack.Screen
-              name="lawyers"
-              options={{
-                headerShown: false,
-                headerBackTitle: "Back",
-                presentation: "modal",
-              }}
-            />
-            <Stack.Screen name="+not-found" />
-          </Stack>
-        </GestureHandlerRootView>
-      </QueryClientProvider>
+      <GestureHandlerRootView>
+        <StatusBar barStyle={"dark-content"} />
+        <Stack>
+          <Stack.Screen
+            name="(tabs)"
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="chat"
+            options={{
+              headerShown: false,
+              animation: "slide_from_bottom",
+              headerBackTitle: "Back",
+            }}
+          />
+          <Stack.Screen
+            name="lawyers"
+            options={{
+              headerShown: false,
+              headerBackTitle: "Back",
+              presentation: "modal",
+            }}
+          />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </GestureHandlerRootView>
     </Auth0Provider>
   );
 }
